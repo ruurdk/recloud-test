cd9dca5f76b3918fbed9b7a05c6b112556b0f6d9
Ruurd Keizer <ruurdk@hotmail.com>
2023-11-23 16:24:04 +0100

    data faker working

--- data/faker/Program.cs
+++ data/faker/Program.cs
@@ -1,4 +1,6 @@
 ﻿using CommandLine;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 
 class Program
 {
@@ -9,12 +11,35 @@ class Program
         Parser.Default.ParseArguments<Options>(args)
             .WithParsed(options =>
             {
-                Console.WriteLine("Generating {0} addresses", options.NumberOfAddresses);
-                var a = new AddressFaker().Generate(options.NumberOfAddresses);
-                
                 Console.WriteLine("Connecting to Redis instance at {0}:{1}", options.Host, options.Port);
+                try {
+                    ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(string.Format("{0}:{1}", options.Host, options.Port));
+                    
+                    var addresses = new List<Address>();
 
+                    for (int i = 0; i < options.NumberOfAddresses; i++) {
+                        
+                        if (i % 1000 == 0)
+                        {
+                            Console.WriteLine("Currently at {0}.", i);
 
+                            // Generate more addresses (much faster in batch).
+                            addresses = new AddressFaker().Generate(1000);                            
+                        }
+                        
+                        var key = string.Format("address:{0}", i);
+                        var a = addresses[i % 1000];
+
+                        var db = redis.GetDatabase();
+                        db.ExecuteAsync(
+                            "JSON.SET", key, "$", JsonConvert.SerializeObject(a)
+                        );                        
+                    }
+                } 
+                catch (RedisConnectionException)
+                {
+                    Console.WriteLine("Unable to connect to Redis server");
+                }
             }
             );   
     }
