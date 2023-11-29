b8931185ad735cf84d11fc7b5d1c8426c73e148d
Ruurd Keizer <ruurdk@hotmail.com>
2023-11-29 15:49:01 +0100

    update faker

--- data/faker/Commands.cs
+++ data/faker/Commands.cs
@@ -5,7 +5,7 @@ public class Options
     [Option('n', Required = true, HelpText = "Number of addresses to generate")]
     public int NumberOfAddresses { get; set; }
 
-    [Option('h', Required = false, HelpText = "Host (FQDN)", Default = "localhost")]
+    [Option('c', Required = false, HelpText = "Host (FQDN)", Default = "localhost")]
     public string? Host { get; set; }
     [Option('p', Required = false, HelpText = "Port number", Default = 6379)]
     public int Port { get; set; }
--- data/faker/README.md
+++ data/faker/README.md
@@ -8,3 +8,5 @@ You need to have dotnet installed, and access to a Redis endpoint.
 
 Then type `dotnet build` to compile, and `dotnet run` and you will be prompted with the command line options.
 
+Hint: to connect to a Redis Enterprise database with a password for default user, you need to use the -s option, in a format like: `redis-19010.internal.c28466.eu-west-1-1.ec2.cloud.rlrcp.com:19010,password=<default_user_password>`
+
